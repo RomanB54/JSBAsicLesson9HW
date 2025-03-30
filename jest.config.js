c30abd0a3291a43785081a0250524379245f0992
RomanB54 <r.barsukov@mgmnt.ru>
2025-03-30 18:06:55 +0300

    fix import html for jest

--- jest.config.js
+++ jest.config.js
@@ -91,6 +91,7 @@ const config = {
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
     '\\.(css|less)$': 'identity-obj-proxy',
+    '\\.html$': './emptyMock.js',
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
